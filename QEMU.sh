962ccf02feab253ef8ce88c3021c244bc1529ade
cccoder123 <51296698+cccoder123@users.noreply.github.com>
2020-10-24 20:51:58 +0100

    Create QEMU.sh

---
+++ QEMU.sh
@@ -0,0 +1,3 @@
+sudo dpkg --add-architecture amd64 && sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install qemu qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinist libvirt-daemon virt-manager
